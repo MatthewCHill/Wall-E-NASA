711518c29cab0d5dae9151894c54080768315c4f
Matthew Hill <matthill265@gmail.com>
2023-02-23 21:23:33 -0700

    Finished my Rover Model

--- Wall-E-NASA/Wall-E-NASA/Model/Rover.swift
+++ Wall-E-NASA/Wall-E-NASA/Model/Rover.swift
@@ -9,4 +9,24 @@ import Foundation
 
 class Rover {
     
+    let earthDay: String
+    let camerName: String
+    let roverImage: String
+    
+    enum Keys: String {
+        
+        case cameraName = "name"
+        case earthDay = "earth_date"
+        case roverImage = "img_src"
+    }
+    
+    init?(roverDict: [String : Any]) {
+        guard let earthDay = roverDict[Keys.earthDay.rawValue] as? String,
+              let roverImage = roverDict[Keys.roverImage.rawValue] as? String,
+              let cameraDict = roverDict["camera"] as? [String : Any],
+              let camerName = cameraDict[Keys.cameraName.rawValue] as? String else {return nil}
+        self.earthDay = earthDay
+        self.roverImage = roverImage
+        self.camerName = camerName
+    }
 }
